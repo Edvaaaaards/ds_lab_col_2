660811112141dd71a8bfa42bfb520a180e806e68
Edvards Daniels Volfs <144360245+Edvaaaaards@users.noreply.github.com>
2024-04-26 16:31:53 +0000

    26.04.2024

--- heap.py
+++ heap.py
@@ -17,7 +17,6 @@ class MaxHeap:
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
-
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
@@ -28,12 +27,10 @@ class MaxHeap:
             left_index = self._left_child(index)
             right_index = self._right_child(index)
 
-            if (left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]):
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
                 max_index = left_index
-
-            if (right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]):
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
                 max_index = right_index
-
             if max_index != index:
                 self._swap(index, max_index)
                 index = max_index
@@ -43,14 +40,11 @@ class MaxHeap:
     def remove(self):
         if len(self.heap) == 0:
             return None
-
         if len(self.heap) == 1:
             return self.heap.pop()
-
         max_value = self.heap[0]
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
-
         return max_value
 
 def find_kth_smallest(nums, k):
@@ -59,7 +53,6 @@ def find_kth_smallest(nums, k):
         max_heap.insert(num)
         if len(max_heap.heap) > k:
             max_heap.remove()
-
     return max_heap.remove()
 
 # Test cases
